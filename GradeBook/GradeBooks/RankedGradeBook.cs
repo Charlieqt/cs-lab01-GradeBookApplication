10e66806fa6627543b50542787dac539deb09695
Charlieqt <karol.kuca.08@gmail.com>
2023-04-04 16:33:15 +0200

    Overrided `RankedGradeBook`'s `GetLetterGrade` method   -Provided the appropriate grades based on how the input grade compares to other students.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,7 @@
-﻿using GradeBook.Enums;
-using GradeBook.GradeBooks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +10,38 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold - 1])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= grades[(threshold * 2) - 1])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= grades[(threshold * 3) - 1])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= grades[(threshold * 4) - 1])
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
